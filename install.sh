fbce64097921f96bccb53ec4380b394210088973
LukeMW <luke@pop-os.localdomain>
2020-12-02 14:57:07 +0000

    Update install.sh
    Added Github Desktop, BleachBit and reordered installs.

--- install.sh
+++ install.sh
@@ -4,21 +4,24 @@
 
 cd ~
 sudo apt update -qq
-sudo apt install -yy gdebi flameshot stacer steam htop notepadqq ghostwriter gparted
+sudo apt install -yy gdebi flameshot stacer steam htop notepadqq ghostwriter gparted bleachbit
 
-# Installing Google Chrome
+# Installing Google Chrome and Github Desktop
 cd /home/luke/Downloads
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo gdebi -n google-chrome-stable_current_amd64.deb
 rm -f google-chrome-stable_current_amd64.deb
+wget https://github.com/shiftkey/desktop/releases/download/release-2.5.7-linux1/GitHubDesktop-linux-2.5.7-linux1.deb
+sudo gdebi -n GitHubDesktop-linux-2.5.7-linux1.deb
+rm -f GitHubDesktop-linux-2.5.7-linux1.deb 
 cd ~
-
-# Adding a repository
-sudo add-apt-repository ppa:libreoffice/libreoffice-7-0
-sudo apt update -qq
 
 # Installing Joplin
 wget -O - https://raw.githubusercontent.com/laurent22/joplin/dev/Joplin_install_and_update.sh | bash
 
 # Installing Planner (flatpak, no prompt)
 flatpak install -y planner
+
+# Adding a repository
+sudo add-apt-repository ppa:libreoffice/libreoffice-7-0
+sudo apt update -qq
